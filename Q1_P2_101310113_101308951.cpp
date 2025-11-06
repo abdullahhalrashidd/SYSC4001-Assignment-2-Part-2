83a3dae2add871a22c40f1a3db09fbee185a96bf
Amin Eldery <amineldery1@gmail.com>
2025-11-06 14:57:54 -0500

    Question 1 Complete

---
+++ Q1_P2_101310113_101308951.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main() {
+    pid_t pid;
+
+    pid = fork();   
+
+    if (pid < 0) {
+        perror("fork failed");
+        exit(1);
+    }
+
+    if (pid == 0) {
+        
+        int counter = 0;
+        while (1) {
+            printf("Child Process (PID=%d) Counter: %d\n", getpid(), counter++);
+            usleep(500000);   
+        }
+    } else {
+        
+        int counter = 0;
+        while (1) {
+            printf("Parent Process (PID=%d) Counter: %d\n", getpid(), counter++);
+            usleep(700000);   
+        }
+    }
+
+    return 0;   
+}
